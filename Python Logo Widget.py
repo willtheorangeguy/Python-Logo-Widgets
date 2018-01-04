856460993576f5d4fc25c7a398cb66b82d90a59f
William <willtheorangeguy@users.noreply.github.com>
2018-01-04 14:48:08 -0800

    Correct file ;)

---
+++ Python Logo Widget.py
@@ -0,0 +1,18 @@
+"""
+(C) 2016 willtheorangeguy. All rights reserved.
+"""
+#Import Statements
+from tkinter import *
+
+#Window Statements
+window = Tk()
+window.title("Python Logo Widget") # Edit title as needed
+
+#Image Statements
+img = PhotoImage(file = "pythonlogogif.gif") # Edit image as needed
+label = Label(window, image = img, bg = "black") # Edit border as needed
+
+# Pack Statements
+label.pack(side = TOP) # Edit pack as needed
+
+window.mainloop()

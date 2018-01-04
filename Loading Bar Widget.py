eb4a4055f76b1b3fb3eb481929cff5a62fec1e77
William <willtheorangeguy@users.noreply.github.com>
2018-01-04 14:42:17 -0800

    Initial code commit

---
+++ Loading Bar Widget.py
@@ -0,0 +1,18 @@
+"""
+(C) 2016 willtheorangeguy. All rights reserved.
+"""
+#Import Statements
+from tkinter import *
+
+#Window Statements
+window = Tk()
+window.title("Loading Bar Widget") # Edit title as needed
+
+#Image Statements
+img = PhotoImage(file = "results-loading.gif") # Edit image as needed
+label = Label(window, image = img,)
+
+# Pack Statements
+label.pack(side = TOP) # Edit pack as needed
+
+window.mainloop()

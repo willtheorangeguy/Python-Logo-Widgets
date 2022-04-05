ab0a163153163f9b907179ee3a68091e7d0f4799
William <willtheorangeguy@users.noreply.github.com>
2022-04-04 17:17:42 -0700

    Update program files and create a new main launcher

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Python Logo Widgets Main Script
+(C) 2022. All rights reserved.
+"""
+# Import Statements
+from tkinter import *
+from logo_widget import *
+from python_powered_length_widget import *
+from python_powered_width_widget import *
+
+def main():
+    logo()
+    python_powered_length()
+    python_powered_width()
+
+if __name__ == "__main__":
+    main()
